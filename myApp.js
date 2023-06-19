bb57b16e58708673302e726924ace50b0d44ed1d
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-06-19 18:49:35 -0300

    root level request middleware

--- myApp.js
+++ myApp.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next()
+})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
-
-app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
     if (process.env["MESSAGE_STYLE"] == "uppercase") {
@@ -15,8 +21,6 @@ app.get('/json', (req, res) => {
         res.json({ "message": "Hello json" });
     }
 });
-
-
 
 
 
