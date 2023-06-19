32897d766949e06def4d2c22858377daeb53fdb6
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-06-18 23:08:20 -0300

    propiedades estaticas middleware

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use('/public', express.static(__dirname + '/public'))
 
 
 
